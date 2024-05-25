d705921a352613a01267f9316f45195b9188bd02
StarkDylan <x.stark.dylan@gmail.com>
2024-05-25 19:17:28 +0800

    push

--- webhook_app.py
+++ webhook_app.py
@@ -7,19 +7,15 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        payload = request.get_json()
-        if payload:
-            # 检查是否是 push 事件
-            if payload.get('ref'):
-                try:
-                    # 拉取最新代码
-                    result = subprocess.run(['git', 'pull origin master --force'], capture_output=True, text=True)
-                    if result.returncode == 0:
-                        return jsonify({'message': 'Pull successful', 'output': result.stdout}), 200
-                    else:
-                        return jsonify({'message': 'Pull failed', 'error': result.stderr}), 500
-                except Exception as e:
-                    return jsonify({'message': 'Exception occurred', 'error': str(e)}), 500
+        try:
+            # 拉取最新代码
+            result = subprocess.run(['git', 'pull origin master --force'], capture_output=True, text=True)
+            if result.returncode == 0:
+                return jsonify({'message': 'Pull successful', 'output': result.stdout}), 200
+            else:
+                return jsonify({'message': 'Pull failed', 'error': result.stderr}), 500
+        except Exception as e:
+            return jsonify({'message': 'Exception occurred', 'error': str(e)}), 500
     return jsonify({'message': 'Invalid request'}), 400
 
 if __name__ == '__main__':
